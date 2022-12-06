ce8022bd463f9f97b7943aa454f89956a37b06fb
Natdanai Kanyakoon <moss10612@gmail.com>
2022-12-06 21:25:57 +0700

    updated css

--- src/components/Header-cart/HeaderCart.js
+++ src/components/Header-cart/HeaderCart.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { MyCartContext } from "../../management/context";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 import "./HeaderCart.css";
 const HeaderCart = () => {
    const { amount } = MyCartContext();
    return (
       <button className="button">
-         <span>ตะกร้าสินค้า</span>
+         <div>
+            <FontAwesomeIcon icon={faCartShopping} />
+         </div>
          <span className="badge">{amount}</span>
       </button>
    );
